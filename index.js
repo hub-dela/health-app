9e7f0fd360e5d0859343e5192053dfd5e1f46eae
Denys Lashchevskyi <68728544+hub-dela@users.noreply.github.com>
2025-07-07 15:16:16 +0200

    chore(index.js): port, paths

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(router);
 router.get("/", index)
 router.get("/health/live", live)
 router.get("/health/ready", ready)
+router.get("/health/startup", startup)
 
 async function index(_req, res) {
     return res.sendStatus(200);
@@ -26,8 +27,12 @@ async function ready(_req, res) {
     return res.sendStatus(200);
 }
 
+async function startup(_req, res) {
+    return res.sendStatus(200);
+}
+
 if (!module.parent) {
-    port = process.env.PORT || 4000;
+    port = process.env.PORT || 3000;
     app.listen(port);
     console.log(`listening on port ${port}`)
 }
